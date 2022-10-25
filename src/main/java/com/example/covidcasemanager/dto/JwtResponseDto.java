55bc6732078096b796b011cb307a032fac648bc8
Bruno Abreu <bruno.abreu@useargo.com>
2022-10-25 13:38:24 -0300

    Adicionado authenticação usando jwt

---
+++ src/main/java/com/example/covidcasemanager/dto/JwtResponseDto.java
@@ -0,0 +1,17 @@
+package com.example.covidcasemanager.dto;
+
+import java.io.Serializable;
+
+public class JwtResponseDto implements Serializable {
+
+    private static final long serialVersionUID = -8091879091924046844L;
+    private final String jwttoken;
+
+    public JwtResponseDto(String jwttoken) {
+        this.jwttoken = jwttoken;
+    }
+
+    public String getToken() {
+        return this.jwttoken;
+    }
+}
